f834de1fe6d53b716699ae5d637c56e62a5ef253
Luis Andrade <andradex.js07@gmail.com>
2024-05-06 19:31:39 -0400

    feat(common): add axios module and pino logger

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,42 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.registerAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        LoggerModule.forRootAsync({
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => {
+            const isProduction =
+              configService.get<string>('NODE_ENV') === 'production';
+            return {
+              exclude: [{ method: RequestMethod.GET, path: 'health' }],
+              pinoHttp: {
+                transport: !isProduction
+                  ? {
+                      target: 'pino-pretty',
+                      options: {
+                        messageKey: 'message',
+                      },
+                    }
+                  : undefined,
+                messageKey: 'message',
+                autoLogging: isProduction,
+              },
+            };
+          },
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
